8bf7ed449085032f1380bc78b74f2b9c95653f6c
xshazks <shahiezasauki15@gmail.com>
2023-03-26 22:39:16 +0700

    go

--- saw_test.go
+++ saw_test.go
@@ -2,13 +2,25 @@ package saw
 
 import (
 	"fmt"
+	"os"
 	"testing"
+
+	"github.com/aiteung/atdb"
 )
+
+var MongoString string = os.Getenv("MONGOSTRING")
+
+var MongoInfo = atdb.DBInfo{
+	DBString: MongoString,
+	DBName:   "dbmonitor",
+}
+
+var MongoConn = atdb.MongoConnect(MongoInfo)
 
 func TestInsertDafdir(t *testing.T) {
 	keterangan := "Masuk"
 	kehadiran := "Hadir"
-	hasil := InsertDafdir(keterangan, kehadiran)
+	hasil := InsertDafdir(MongoConn, keterangan, kehadiran)
 	fmt.Println(hasil)
 }
 
@@ -16,20 +28,20 @@ func TestInsertNilai(t *testing.T) {
 	matpel := "Matematika"
 	kkm := "70"
 	nilai := "90"
-	hasil := InsertNilai(matpel, kkm, nilai)
+	hasil := InsertNilai(MongoConn, matpel, kkm, nilai)
 	fmt.Println(hasil)
 }
 
 func TestInsertDafpel(t *testing.T) {
 	keterangan := "Terlambat"
 	poin := "-2"
-	hasil := InsertDafpel(keterangan, poin)
+	hasil := InsertDafpel(MongoConn, keterangan, poin)
 	fmt.Println(hasil)
 }
 
 func TestInsertPembayaran(t *testing.T) {
 	keterangan := "Pembayaran UTS"
 	status := "Terbayar"
-	hasil := InsertPembayaran(keterangan, status)
+	hasil := InsertPembayaran(MongoConn, keterangan, status)
 	fmt.Println(hasil)
 }
