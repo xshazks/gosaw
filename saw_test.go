2915ea5e0349e84e8d119f1824728cc2a309104a
xshazks <shahiezasauki15@gmail.com>
2023-03-27 05:08:29 +0700

    vv

--- saw_test.go
+++ saw_test.go
@@ -15,30 +15,30 @@ var MongoInfo = atdb.DBInfo{
 var MongoConn = atdb.MongoConnect(MongoInfo)
 
 func TestInsertDafdir(t *testing.T) {
-	keterangan := "Masuk"
-	kehadiran := "Hadir"
+	keterangan := "Sakit"
+	kehadiran := "Tidak Hadir"
 	hasil := InsertDafdir(MongoConn, keterangan, kehadiran)
 	fmt.Println(hasil)
 }
 
 func TestInsertNilai(t *testing.T) {
-	matpel := "Matematika"
+	matpel := "Biologi"
 	kkm := "70"
-	nilai := "90"
+	nilai := "80"
 	hasil := InsertNilai(MongoConn, matpel, kkm, nilai)
 	fmt.Println(hasil)
 }
 
 func TestInsertDafpel(t *testing.T) {
-	keterangan := "Terlambat"
-	poin := "-2"
+	keterangan := "Atribut Tidak Lengkap"
+	poin := "-1"
 	hasil := InsertDafpel(MongoConn, keterangan, poin)
 	fmt.Println(hasil)
 }
 
 func TestInsertPembayaran(t *testing.T) {
-	keterangan := "Pembayaran UTS"
-	status := "Terbayar"
+	keterangan := "Pembayaran SPP"
+	status := "Belum bayar"
 	hasil := InsertPembayaran(MongoConn, keterangan, status)
 	fmt.Println(hasil)
 }
